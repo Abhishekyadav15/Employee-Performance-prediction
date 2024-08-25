c0c1b594932959c32b79e4c810993b459ddcedb6
Abhishekyadav15 <132095891+Abhishekyadav15@users.noreply.github.com>
2024-08-25 13:18:41 +0530

    Add files via upload

---
+++ project/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# Load the model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Assuming the input data comes from a form and is a list of floats
+    input_data = [float(x) for x in request.form.values()]
+    
+    # Perform the prediction
+    p = model.predict([input_data])
+    if(p>0.75):
+        prediction="highly"
+    elif(p <0.60):
+        prediction="less"
+    else:
+        prediction="medium"    
+    
+    # You can pass the prediction result to the submit.html page
+    return render_template('submit.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
